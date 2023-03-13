da369dc8d92eea2edd3fb8ab9cbc9ce1bb97d5f3
r20222 <101579892+r20222@users.noreply.github.com>
2023-03-13 19:42:08 +0100

    mobile-first overlay menu

--- public/script.js
+++ public/script.js
@@ -1,8 +1,20 @@
 const menu = document.querySelector('.menu-button')
 const overlayContent = document.querySelector('.overlay-content')
+const html = document.querySelector('html')
+const article = document.querySelector('article')
+const clickAwayMenu = document.querySelector('.click-away-menu')
 
 menu.addEventListener('click', menuContent)
+clickAwayMenu.addEventListener('click', menuAway)
 
 function menuContent(){
-    overlayContent.classList.toggle('visible-menu-overlay-content')
+    overlayContent.classList.add('visible-menu-overlay-content')
+    html.classList.add('overflow-y')
+    article.classList.add('pointer-event-none')
+}
+
+function menuAway(){
+    overlayContent.classList.remove('visible-menu-overlay-content')
+    html.classList.remove('overflow-y')
+    article.classList.remove('pointer-event-none')
 }
