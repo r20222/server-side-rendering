ce6a7633d2619cdaeba90d79159df4c7019d3b25
r20222 <101579892+r20222@users.noreply.github.com>
2023-03-16 17:38:04 +0100

    tips slider

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+
+// overlay menu
 const menu = document.querySelector('.menu-button')
 const overlayContent = document.querySelector('.overlay-content')
 const html = document.querySelector('html')
@@ -18,3 +20,45 @@ function menuAway(){
     html.classList.remove('overflow-y')
     article.classList.remove('pointer-event-none')
 }
+
+
+
+// tips slide
+
+// definieer de variabelen
+const buttonLeft = document.querySelector('.button-tips-left')
+const buttonRight = document.querySelector('.button-tips-right')
+
+// laat de slide bij de eerste beginnen
+let slideIndex = 1
+
+// laat de functie showdivs in actie treden (als de slideindex 1 is, zie je de eerste 'tip').
+showDivs(slideIndex);
+
+// Geef de knoppen een eventlistener
+buttonLeft.addEventListener('click', function() { plusClick(-1) });
+buttonRight.addEventListener('click', function() { plusClick(1) });
+
+// deze functie wordt uitgevoerd wanneer je op de button klikt, 
+// Met showDivs(slideIndex += n); geeft die aan of de slideindex een pagina verder of terug moet gaan.
+function plusClick(n){
+    showDivs(slideIndex += n);
+    console.log('click')
+}
+
+// Deze onderstaand functie showdivs besluit welke 'tip' te zien is.
+// n staat voor de slideindex waarde
+// x staat voor de slides
+// met de i wordt er een loop uitgevoerd, i gaat gewoon getallen langs, en zolang het getal kleiner is dan
+// de lengte van het aantal slider wordt er een nummer bij opgeteld
+// [slideIndex-1] wordt gebruikt omdat bijvoorbeeld +3 eigenlijk overeenzou komen met index 2.
+function showDivs(n) {
+    let i;
+    let x = document.getElementsByClassName("tips-slider");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    x[slideIndex-1].style.display = "flex";  
+  }
